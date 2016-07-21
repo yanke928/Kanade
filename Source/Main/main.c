3b199b8af53213da71350ac822cb8a830bf7dfbb
yanke928 <yankeyyanke@qq.com>
2016-07-21 17:55:47 +0800

    Add bootloader project

--- Source/Main/main.c
+++ Source/Main/main.c
@@ -1,16 +1,6 @@
 #include "stm32f10x.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
-
 #include "stm32f10x_rcc.h"
 #include "misc.h"
-
-#include "led.h"
-
-#include "Startup.h"
-
-#include <string.h>
 
 #define USER_APP_OFFSET 0x00002000
 
@@ -25,7 +15,6 @@ __asm void MSR_MSP(u32 addr) //设置堆栈指针
 int main(void)
 { 
  STM32_Init();
- SystemStart();
 }
 
 
