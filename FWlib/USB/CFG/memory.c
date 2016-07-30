593d59aafc9aff62684248aceaed100a5a19734c
yanke928 <yankeyyanke@qq.com>
2016-07-30 13:24:23 +0800

    Update memory to 4.0.0

--- FWlib/USB/CFG/memory.c
+++ FWlib/USB/CFG/memory.c
@@ -1,17 +1,30 @@
-/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
-* File Name          : memory.c
-* Author             : MCD Application Team
-* Version            : V2.2.0
-* Date               : 06/13/2008
-* Description        : Memory management layer
-********************************************************************************
-* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*******************************************************************************/
+/**
+  ******************************************************************************
+  * @file    memory.c
+  * @author  MCD Application Team
+  * @version V4.0.0
+  * @date    21-January-2013
+  * @brief   Memory management layer
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
 
 /* Includes ------------------------------------------------------------------*/
 
@@ -23,29 +36,30 @@
 #include "usb_conf.h"
 #include "hw_config.h"
 #include "mass_mal.h"
+#include "usb_lib.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-vu32 Block_Read_count = 0;
-vu32 Block_offset;
-vu32 Counter = 0;
-
-u32  Idx;
-u32 Data_Buffer[BULK_MAX_PACKET_SIZE *16]; /* 512 bytes*/
-u8 TransferState = TXFR_IDLE;
+__IO uint32_t Block_Read_count = 0;
+__IO uint32_t Block_offset;
+__IO uint32_t Counter = 0;
+uint32_t  Idx;
+uint32_t Data_Buffer[BULK_MAX_PACKET_SIZE *16]; /* 512 bytes*/
+uint8_t TransferState = TXFR_IDLE;
 /* Extern variables ----------------------------------------------------------*/
-extern u8 Bulk_Data_Buff[BULK_MAX_PACKET_SIZE];  /* data buffer*/
-extern u16 Data_Len;
-extern u8 Bot_State;
+extern uint8_t Bulk_Data_Buff[BULK_MAX_PACKET_SIZE];  /* data buffer*/
+extern uint16_t Data_Len;
+extern uint8_t Bot_State;
 extern Bulk_Only_CBW CBW;
 extern Bulk_Only_CSW CSW;
-extern u32 Mass_Memory_Size[2];
-extern u32 Mass_Block_Size[2];
+extern uint32_t Mass_Memory_Size[2];
+extern uint32_t Mass_Block_Size[2];
 
-extern u8 Usb_Status_Reg;
-
+/* Private function prototypes -----------------------------------------------*/
+/* Extern function prototypes ------------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
 
 /*******************************************************************************
 * Function Name  : Read_Memory
@@ -54,51 +68,56 @@ extern u8 Usb_Status_Reg;
 * Output         : None.
 * Return         : None.
 *******************************************************************************/
-void Read_Memory(u8 lun, u32 Memory_Offset, u32 Transfer_Length)
+void Read_Memory(uint8_t lun, uint32_t Memory_Offset, uint32_t Transfer_Length)
 {
-	static u32 Offset, Length;
-	u8 STA;
-	
-	if (TransferState == TXFR_IDLE )
-	{
-		Offset = Memory_Offset * Mass_Block_Size[lun];
-		Length = Transfer_Length * Mass_Block_Size[lun];
-		TransferState = TXFR_ONGOING;
-	}
-	if (TransferState == TXFR_ONGOING )
-	{
-		if (!Block_Read_count)
-		{
-			STA=MAL_Read(lun ,
-					     Offset ,
-			   	  		 Data_Buffer,
-		    	   	 	 Mass_Block_Size[lun]);
-			if(STA)Usb_Status_Reg|=0X08;
-			UserToPMABufferCopy((u8 *)Data_Buffer, ENDP1_TXADDR, BULK_MAX_PACKET_SIZE);
-			Block_Read_count = Mass_Block_Size[lun] - BULK_MAX_PACKET_SIZE;
-			Block_offset = BULK_MAX_PACKET_SIZE;
-		}
-		else
-		{
-			UserToPMABufferCopy((u8 *)Data_Buffer + Block_offset, ENDP1_TXADDR, BULK_MAX_PACKET_SIZE);
-			Block_Read_count -= BULK_MAX_PACKET_SIZE;
-			Block_offset += BULK_MAX_PACKET_SIZE;
-		}	  
-		SetEPTxCount(ENDP1, BULK_MAX_PACKET_SIZE);
-		SetEPTxStatus(ENDP1, EP_TX_VALID);
-		Offset += BULK_MAX_PACKET_SIZE;
-		Length -= BULK_MAX_PACKET_SIZE;
-		
-		CSW.dDataResidue -= BULK_MAX_PACKET_SIZE;
-	}
-	if (Length == 0)
-	{
-		Block_Read_count = 0;
-		Block_offset = 0;
-		Offset = 0;
-		Bot_State = BOT_DATA_IN_LAST;
-		TransferState = TXFR_IDLE;
-	}					 
+  static uint32_t Offset, Length;
+
+  if (TransferState == TXFR_IDLE )
+  {
+    Offset = Memory_Offset * Mass_Block_Size[lun];
+    Length = Transfer_Length * Mass_Block_Size[lun];
+    TransferState = TXFR_ONGOING;
+  }
+
+  if (TransferState == TXFR_ONGOING )
+  {
+    if (!Block_Read_count)
+    {
+      MAL_Read(lun ,
+               Offset ,
+               Data_Buffer,
+               Mass_Block_Size[lun]);
+
+      USB_SIL_Write(EP1_IN, (uint8_t *)Data_Buffer, BULK_MAX_PACKET_SIZE);
+
+      Block_Read_count = Mass_Block_Size[lun] - BULK_MAX_PACKET_SIZE;
+      Block_offset = BULK_MAX_PACKET_SIZE;
+    }
+    else
+    {
+      USB_SIL_Write(EP1_IN, (uint8_t *)Data_Buffer + Block_offset, BULK_MAX_PACKET_SIZE);
+
+      Block_Read_count -= BULK_MAX_PACKET_SIZE;
+      Block_offset += BULK_MAX_PACKET_SIZE;
+    }
+
+    SetEPTxCount(ENDP1, BULK_MAX_PACKET_SIZE);
+    SetEPTxStatus(ENDP1, EP_TX_VALID);  
+    Offset += BULK_MAX_PACKET_SIZE;
+    Length -= BULK_MAX_PACKET_SIZE;
+
+    CSW.dDataResidue -= BULK_MAX_PACKET_SIZE;
+    Led_RW_ON();
+  }
+  if (Length == 0)
+  {
+    Block_Read_count = 0;
+    Block_offset = 0;
+    Offset = 0;
+    Bot_State = BOT_DATA_IN_LAST;
+    TransferState = TXFR_IDLE;
+    Led_RW_OFF();
+  }
 }
 
 /*******************************************************************************
@@ -108,55 +127,52 @@ void Read_Memory(u8 lun, u32 Memory_Offset, u32 Transfer_Length)
 * Output         : None.
 * Return         : None.
 *******************************************************************************/
-void Write_Memory (u8 lun, u32 Memory_Offset, u32 Transfer_Length)
-{	 
-	static u32 W_Offset, W_Length;	
-	u8 STA;   
-	u32 temp =  Counter + 64;	  
-	if (TransferState == TXFR_IDLE )
-	{
-		W_Offset = Memory_Offset * Mass_Block_Size[lun];
-		W_Length = Transfer_Length * Mass_Block_Size[lun];
-		TransferState = TXFR_ONGOING;
-	}	   
-	if (TransferState == TXFR_ONGOING )
-	{   
-		for (Idx = 0 ; Counter < temp; Counter++)
-		{
-			*((u8 *)Data_Buffer + Counter) = Bulk_Data_Buff[Idx++];
-		}  
-		W_Offset += Data_Len;
-		W_Length -= Data_Len;	  
-		if (!(W_Length % Mass_Block_Size[lun]))
-		{
-			Counter = 0;
-			STA=MAL_Write(lun ,
-					  	  W_Offset - Mass_Block_Size[lun],
-					  	  Data_Buffer,
-					  	  Mass_Block_Size[lun]);
-			if(STA)Usb_Status_Reg|=0X04;
-		}				  
-		CSW.dDataResidue -= Data_Len;
-		SetEPRxStatus(ENDP2, EP_RX_VALID); /* enable the next transaction*/		 
-	}	   
-	if ((W_Length == 0) || (Bot_State == BOT_CSW_Send))
-	{
-		Counter = 0;
-		Set_CSW (CSW_CMD_PASSED, SEND_CSW_ENABLE);
-		TransferState = TXFR_IDLE;
-		Led_RW_OFF();
-	}
-} 
+void Write_Memory (uint8_t lun, uint32_t Memory_Offset, uint32_t Transfer_Length)
+{
 
+  static uint32_t W_Offset, W_Length;
 
+  uint32_t temp =  Counter + 64;
 
+  if (TransferState == TXFR_IDLE )
+  {
+    W_Offset = Memory_Offset * Mass_Block_Size[lun];
+    W_Length = Transfer_Length * Mass_Block_Size[lun];
+    TransferState = TXFR_ONGOING;
+  }
 
+  if (TransferState == TXFR_ONGOING )
+  {
 
+    for (Idx = 0 ; Counter < temp; Counter++)
+    {
+      *((uint8_t *)Data_Buffer + Counter) = Bulk_Data_Buff[Idx++];
+    }
 
+    W_Offset += Data_Len;
+    W_Length -= Data_Len;
 
+    if (!(W_Length % Mass_Block_Size[lun]))
+    {
+      Counter = 0;
+      MAL_Write(lun ,
+                W_Offset - Mass_Block_Size[lun],
+                Data_Buffer,
+                Mass_Block_Size[lun]);
+    }
 
+    CSW.dDataResidue -= Data_Len;
+    SetEPRxStatus(ENDP2, EP_RX_VALID); /* enable the next transaction*/   
+    Led_RW_ON();
+  }
 
-
-
-
+  if ((W_Length == 0) || (Bot_State == BOT_CSW_Send))
+  {
+    Counter = 0;
+    Set_CSW (CSW_CMD_PASSED, SEND_CSW_ENABLE);
+    TransferState = TXFR_IDLE;
+    Led_RW_OFF();
+  }
+}
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
 
